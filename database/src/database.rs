62de6112d76e5580dc011e5014107a155783fd98
Shem Sedrick <ssedrick1@gmail.com>
2020-12-29 07:45:37 -0600

    Set up db part of listening

--- database/src/database.rs
+++ database/src/database.rs
@@ -1,5 +1,8 @@
 use crate::config::Config;
+use log::info;
+use syntax;
 use syntax::document::Document;
+use tokio::sync::{mpsc::Receiver, oneshot};
 
 pub(crate) struct Database {
     schema: Document,
@@ -10,6 +13,20 @@ impl Database {
     pub fn new(_config: &Config) -> Self {
         Self {
             schema: Document::default(),
+        }
+    }
+
+    pub async fn run(&mut self, mut command: Receiver<(String, oneshot::Sender<String>)>) {
+        while let Some((gql_str, response)) = command.recv().await {
+            // handle connection
+            tokio::spawn(async move {
+                let parsed = syntax::parse(&gql_str);
+                println!("Parsed: {:?}", parsed);
+                match response.send("Received input".into()) {
+                    Ok(()) => info!("Response sent successfully"),
+                    Err(e) => info!("Response from db failed: {}", e),
+                };
+            });
         }
     }
 }
--- database/src/listener.rs
+++ database/src/listener.rs
@@ -5,10 +5,11 @@ use log::info;
 use net::handlers;
 use std::time::Duration;
 use tokio::runtime::Builder;
+use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinHandle;
 
 pub(crate) fn listen(
-    _database: Database,
+    mut database: Database,
     config: &Config,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut runtime = Builder::new()
@@ -21,12 +22,19 @@ pub(crate) fn listen(
 
     let mut sockets: Vec<JoinHandle<Result<(), std::io::Error>>> = Vec::new();
 
+    let (db_command, db_receiver) = mpsc::channel::<(String, oneshot::Sender<String>)>(64);
+    let _handle = runtime.handle().spawn(async move {
+        database.run(db_receiver).await;
+    });
+
     for protocol in &config.protocols {
         info!("setting up protocol: {}", protocol);
         match protocol.as_str() {
             "tcp" => {
+                let sender = db_command.clone();
                 let handle = runtime.handle();
-                let join_handle = handle.spawn(async move { handlers::handle_tcp(9874).await });
+                let join_handle =
+                    handle.spawn(async move { handlers::handle_tcp(9874, sender).await });
                 sockets.push(join_handle);
             }
             _ => println!("Protocol not supported: {}", protocol),
